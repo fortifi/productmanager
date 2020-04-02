7cac4f74d98eaf9926c502ba0be0ed47d6004278
Tom Kay <oridan82@gmail.com>
2020-04-02 21:23:50 +0100

    availability reserve response

--- src/Response/AvailabilityReserveResponse.php
+++ src/Response/AvailabilityReserveResponse.php
@@ -3,6 +3,11 @@ namespace Fortifi\ProductManager\Response;
 
 class AvailabilityReserveResponse extends Response
 {
+  /**
+   * @var bool
+   */
+  public $reserved;
+
   public function __construct()
   {
     $this->type = ResponseType::AVAILABILITY_RESERVE();
