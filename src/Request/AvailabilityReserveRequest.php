ef891e27f0607272e4b9a9a88537605412e427b6
Tom Kay <oridan82@gmail.com>
2020-04-03 18:18:42 +0100

    add reserveTtl

--- src/Request/AvailabilityReserveRequest.php
+++ src/Request/AvailabilityReserveRequest.php
@@ -3,6 +3,8 @@ namespace Fortifi\ProductManager\Request;
 
 class AvailabilityReserveRequest extends AvailabilityRequest
 {
+  public $reserveTtl;
+
   public function __construct()
   {
     $this->type = RequestType::AVAILABILITY_RESERVE();
