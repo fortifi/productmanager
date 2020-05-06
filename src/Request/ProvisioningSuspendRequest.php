3dc36e3c936544906dcf83147b15f8d2b1f0334b
Brooke Bryan <brooke@bajb.net>
2020-05-06 23:40:16 +0100

    Add suspend message

--- src/Request/ProvisioningSuspendRequest.php
+++ src/Request/ProvisioningSuspendRequest.php
@@ -3,6 +3,9 @@ namespace Fortifi\ProductManager\Request;
 
 class ProvisioningSuspendRequest extends ProvisioningRequest
 {
+  /** @var string Reason to display to the customer for the suspension */
+  public $message;
+
   public function __construct()
   {
     $this->type = RequestType::PROVISION_SUSPEND();
