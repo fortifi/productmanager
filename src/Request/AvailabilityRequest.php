ea4bc44335d7ba7dde33019cc40740db7de593ab
Brooke Bryan <brooke@bajb.net>
2020-08-14 14:16:05 +0100

    Add properties to Availability Requests

--- src/Request/AvailabilityRequest.php
+++ src/Request/AvailabilityRequest.php
@@ -1,7 +1,28 @@
 <?php
 namespace Fortifi\ProductManager\Request;
 
+use Fortifi\ProductManager\DataType\TransportProperty;
+use Packaged\Helpers\Objects;
+
 abstract class AvailabilityRequest extends Request
 {
+  /** @var TransportProperty[] */
+  public $properties = [];
+
+  public function hydrate($from)
+  {
+    Objects::hydrate($this, $from);
+    if(!empty($this->properties))
+    {
+      $newProps = [];
+      foreach($this->properties as $prop)
+      {
+        $newProps[] = Objects::hydrate(new TransportProperty(), $prop);
+      }
+      $this->properties = $newProps;
+    }
+    return $this;
+  }
+
   public $reserveKey;
 }
