39a3f335d460dbae1dc241b68d6ba115c9aec023
Brooke Bryan <brooke@bajb.net>
2020-08-18 13:38:59 +0100

    Show auth token and data

--- src/Response/ConfigurationResponse.php
+++ src/Response/ConfigurationResponse.php
@@ -3,6 +3,7 @@
 namespace Fortifi\ProductManager\Response;
 
 use Fortifi\ProductManager\DataType\ProductDefinition;
+use Fortifi\ProductManager\DataType\PropertyDefinition;
 
 class ConfigurationResponse extends Response
 {
@@ -10,6 +11,12 @@ class ConfigurationResponse extends Response
    * @var ProductDefinition[]
    */
   public $products = [];
+
+  public $authTokenName = '';
+  /**
+   * @var PropertyDefinition[]
+   */
+  public $authData = [];
 
   public function __construct()
   {
