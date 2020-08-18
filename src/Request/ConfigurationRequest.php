a6ee9b8cc5136490f16d2b4c46a7012471727e09
Brooke Bryan <brooke@bajb.net>
2020-08-18 15:52:52 +0100

    Include sku data

--- src/Request/ConfigurationRequest.php
+++ src/Request/ConfigurationRequest.php
@@ -3,6 +3,11 @@ namespace Fortifi\ProductManager\Request;
 
 class ConfigurationRequest extends Request
 {
+  /**
+   * @var bool
+   */
+  public $includeSkuData;
+
   public function __construct()
   {
     $this->type = RequestType::CONFIGURATION();
