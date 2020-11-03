c2b23b1c4eb50459c9098c3c0e019f3f6bae1290
Steve Barrow <steve@infurio.com>
2020-11-03 15:40:01 +0000

    Added modifiedChild to ProvisioningNotifyRequest

--- src/Request/ProvisioningNotifyRequest.php
+++ src/Request/ProvisioningNotifyRequest.php
@@ -1,13 +1,18 @@
 <?php
 namespace Fortifi\ProductManager\Request;
 
+use Fortifi\ProductManager\DataType\PurchasedProduct;
+
 class ProvisioningNotifyRequest extends ProvisioningRequest
 {
   const REASON_UNKNOWN = '';
   const REASON_CHILD_ADDED = 'child.added';
   const REASON_CHILD_REMOVED = 'child.removed';
 
+  /** @var string */
   public $reason;
+  /** @var PurchasedProduct */
+  public $modifiedChild;
 
   public function __construct()
   {
