7e3203642fcb407ab4ab0f71ab7eb7df85e0c19a
Steve Barrow <steve@infurio.com>
2020-11-05 12:18:06 +0000

    added renew and notify to Request.php

--- src/Request/Request.php
+++ src/Request/Request.php
@@ -29,41 +29,47 @@ class Request extends BaseData
 
     switch($raw->type)
     {
+      case RequestType::AVAILABILITY_CHECK:
+        $request = new AvailabilityCheckRequest();
+        break;
+      case RequestType::AVAILABILITY_RESERVE:
+        $request = new AvailabilityReserveRequest();
+        break;
       case RequestType::CONFIGURATION:
         $request = new ConfigurationRequest();
         break;
       case RequestType::HEALTH_CHECK:
         $request = new HealthCheckRequest();
         break;
-      case RequestType::PROVISION_SETUP:
-        $request = new ProvisioningSetupRequest();
-        break;
       case RequestType::PROVISION_ACTIVATE:
         $request = new ProvisioningActivateRequest();
-        break;
-      case RequestType::PROVISION_PROPERTIES_SET:
-        $request = new ProvisioningPropertiesSetRequest();
-        break;
-      case RequestType::PROVISION_MODIFY:
-        $request = new ProvisioningModifyRequest();
-        break;
-      case RequestType::PROVISION_SUSPEND:
-        $request = new ProvisioningSuspendRequest();
-        break;
-      case RequestType::PROVISION_REACTIVATE:
-        $request = new ProvisioningReactivateRequest();
         break;
       case RequestType::PROVISION_CANCEL:
         $request = new ProvisioningCancelRequest();
         break;
+      case RequestType::PROVISION_MODIFY:
+        $request = new ProvisioningModifyRequest();
+        break;
+      case RequestType::PROVISION_NOTIFY:
+        $request = new ProvisioningNotifyRequest();
+        break;
+      case RequestType::PROVISION_PROPERTIES_SET:
+        $request = new ProvisioningPropertiesSetRequest();
+        break;
+      case RequestType::PROVISION_REACTIVATE:
+        $request = new ProvisioningReactivateRequest();
+        break;
+      case RequestType::PROVISION_RENEW:
+        $request = new ProvisioningRenewRequest();
+        break;
+      case RequestType::PROVISION_SETUP:
+        $request = new ProvisioningSetupRequest();
+        break;
+      case RequestType::PROVISION_SUSPEND:
+        $request = new ProvisioningSuspendRequest();
+        break;
       case RequestType::PROVISION_TERMINATE:
         $request = new ProvisioningTerminateRequest();
-        break;
-      case RequestType::AVAILABILITY_CHECK:
-        $request = new AvailabilityCheckRequest();
-        break;
-      case RequestType::AVAILABILITY_RESERVE:
-        $request = new AvailabilityReserveRequest();
         break;
       default:
         $request = new static();
