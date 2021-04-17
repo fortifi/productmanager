abe5f8030f25d57da74ac8111d1c8e2493633729
Tom Kay <oridan82@gmail.com>
2021-04-17 20:12:43 +0100

    support minimumDelay on ProvisioningRetry

--- src/Response/ProvisioningRetryResponse.php
+++ src/Response/ProvisioningRetryResponse.php
@@ -3,6 +3,11 @@ namespace Fortifi\ProductManager\Response;
 
 class ProvisioningRetryResponse extends ProvisioningResponse
 {
+  /**
+   * @var int
+   */
+  public $minimumDelay;
+
   public function __construct()
   {
     $this->type = ResponseType::PROVISION_RETRY();
